32f80a797c26132349aab851f8eec17c6c728f09
Hyung Won Lee <hwlee@inje.ac.kr>
2025-06-09 14:53:14 +0900

    add time measuring code

--- CUDA_Tutorial/day01/add_vector_combine_size.cu
+++ CUDA_Tutorial/day01/add_vector_combine_size.cu
@@ -6,10 +6,11 @@
 
 #define RANDOM_RANGE 500
 
-__global__ void add(int *a, int *b, int *c)
+__global__ void add(int *a, int *b, int *c, int n)
 {
   int index = threadIdx.x + blockIdx.x * blockDim.x;
-  c[index] = a[index] + b[index];
+  if (index < n)
+    c[index] = a[index] + b[index];
 }
 
 void random_ints(int *a, int n)
@@ -17,6 +18,22 @@ void random_ints(int *a, int n)
   for(int i = 0; i<n; i++)
   {
     a[i] = rand()%RANDOM_RANGE;
+  }
+}
+
+void print_elapsed(double elapsed_time)
+{
+  if (elapsed_time < 1.0e-5)
+  {
+    printf("Elapsed time : %.3f micro seconds.\n", elapsed_time*1.0e6);
+  }
+  else if (elapsed_time < 1.0e-2)
+  {
+    printf("Elapsed time : %.3f milli seconds.\n", elapsed_time*1.0e3);
+  }
+  else
+  {
+    printf("Elapsed time : %.3f seconds.\n", elapsed_time);
   }
 }
 
@@ -29,6 +46,8 @@ int main()
   int *d_a, *d_b, *d_c;		// device copies of a, b, c
   int size = N * sizeof(int);	// array size in bytes
   int k;
+  clock_t start, end;
+  double elapsed_time;
 
   // Intialize random seed
   srand((unsigned int)time(NULL));
@@ -41,15 +60,19 @@ int main()
   b = (int *)malloc(size); random_ints(b, N);
   c = (int *)malloc(size);
   // Copy inputs to device
+  start = clock();
   cudaMemcpy(d_a, a, size, cudaMemcpyHostToDevice);
   cudaMemcpy(d_b, b, size, cudaMemcpyHostToDevice);
   // Launch add() kernel on GPU with N threads
-  add<<<N/THREADS_PER_BLOCK, THREADS_PER_BLOCK>>>(d_a, d_b, d_c);
+  add<<<N/THREADS_PER_BLOCK, THREADS_PER_BLOCK>>>(d_a, d_b, d_c, N);
   // Copy result back to host
   cudaMemcpy(c, d_c, size, cudaMemcpyDeviceToHost);
+  end = clock();
   // Print the result
   k = rand()%N;
-  printf("Result: c[%d] is %d, a[%d] = %d, b[%d] = %d\n", k, c[k], k, a[k], k, b[k]);
+  printf("Result: Size = %d, c[%d] = %d, a[%d] = %d, b[%d] = %d\n", N, k, c[k], k, a[k], k, b[k]);
+  elapsed_time = ((double)(end - start)) / CLOCKS_PER_SEC;
+  print_elapsed(elapsed_time);
   // Cleanup
   free(a); free(b); free(c);
   cudaFree(d_a); cudaFree(d_b); cudaFree(d_c);
