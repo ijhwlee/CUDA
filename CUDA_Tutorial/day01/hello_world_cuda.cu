ab020077cb4856fc83f43892510a7b5c2dd5e273
Hyung Won Lee <hwlee@inje.ac.kr>
2025-08-11 15:10:09 +0900

    modify for device printf function

--- CUDA_Tutorial/day01/hello_world_cuda.cu
+++ CUDA_Tutorial/day01/hello_world_cuda.cu
@@ -4,12 +4,13 @@
 
 __global__ void mykernel(void)
 {
-  printf("Hello World from Device code!\n");
+  printf("Hello World from Device code! blockIdx.x = %d, threadIdx.x = %d\n", blockIdx.x, threadIdx.x);
 }
 
 int main()
 {
-  mykernel<<<1,1>>>();
+  mykernel<<<5,5>>>();
+  cudaDeviceSynchronize();  // Ensure all output is flushed
   printf("Hello World!\n");
   return 0;
 }
